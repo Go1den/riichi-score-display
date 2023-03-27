13598aed83e14adaade48caa1119692e8963bec5
Golden <gooneden@gmail.com>
2023-03-27 02:23:20 -0500

    Made the editing of field values simpler and cleaned up js code for reusability

--- main.js
+++ main.js
@@ -18,31 +18,60 @@ function endGame() {
     document.getElementById("round").innerHTML = 'Final';
 }
 
-function newGame() {
+function resetInputScores() {
+    document.getElementById("inputP1Score").value = '25000';
+    document.getElementById("inputP2Score").value = '25000';
+    document.getElementById("inputP3Score").value = '25000';
+    document.getElementById("inputP4Score").value = '25000';
+}
+
+function resetInputNames() {
+    document.getElementById("inputP1Name").value = 'East';
+    document.getElementById("inputP2Name").value = 'South';
+    document.getElementById("inputP3Name").value = 'West';
+    document.getElementById("inputP4Name").value = 'North';
+}
+
+function resetRound() {
     document.getElementById("repeat").innerHTML = '';
     document.getElementById("round").innerHTML = 'East 1';
+}
+
+function resetScores() {
     document.getElementById("1Score").innerHTML = '25000';
     document.getElementById("2Score").innerHTML = '25000';
     document.getElementById("3Score").innerHTML = '25000';
     document.getElementById("4Score").innerHTML = '25000';
-    updateDealerInd();
 }
 
-function reset() {
-    document.getElementById("repeat").innerHTML = '';
-    document.getElementById("round").innerHTML = 'East 1';
+function resetNames() {
     document.getElementById("1Name").innerHTML = 'East';
     document.getElementById("2Name").innerHTML = 'South';
     document.getElementById("3Name").innerHTML = 'West';
     document.getElementById("4Name").innerHTML = 'North';
-    document.getElementById("1Score").innerHTML = '25000';
-    document.getElementById("2Score").innerHTML = '25000';
-    document.getElementById("3Score").innerHTML = '25000';
-    document.getElementById("4Score").innerHTML = '25000';
+}
+
+function resetNametags() {
     document.getElementById("1Nametag").innerHTML = 'East';
     document.getElementById("2Nametag").innerHTML = 'South';
     document.getElementById("3Nametag").innerHTML = 'West';
     document.getElementById("4Nametag").innerHTML = 'North';
+}
+
+function newGame() {
+    resetRound();
+    resetScores();
+    resetInputScores();
+    updateDealerInd();
+}
+
+function reset() {
+    resetRound();
+    resetNames();
+    resetScores();
+    resetNametags();
+    resetInputScores();
+    resetInputNames();
     updateDealerInd();
 }
 
@@ -81,17 +110,19 @@ function getNextWind(currentWind) {
     }
 }
 
-function updateField() {
-    let data = document.getElementById("data");
-    let selectField = document.getElementById("selectField").value;
-    let selectPlayer = document.getElementById("selectPlayer").value;
-    let updateField = document.getElementById(selectPlayer + selectField);
-    updateField.innerHTML = data.value;
-    if (selectField === 'Name') {
-        let nextUpdateField = document.getElementById(selectPlayer + selectField + "tag");
-        nextUpdateField.innerHTML = data.value;
-    }
-    data.value = '';
+function updateFields() {
+    document.getElementById("1Name").innerHTML = document.getElementById("inputP1Name").value;
+    document.getElementById("1Nametag").innerHTML = document.getElementById("inputP1Name").value;
+    document.getElementById("2Name").innerHTML = document.getElementById("inputP2Name").value;
+    document.getElementById("2Nametag").innerHTML = document.getElementById("inputP2Name").value;
+    document.getElementById("3Name").innerHTML = document.getElementById("inputP3Name").value;
+    document.getElementById("3Nametag").innerHTML = document.getElementById("inputP3Name").value;
+    document.getElementById("4Name").innerHTML = document.getElementById("inputP4Name").value;
+    document.getElementById("4Nametag").innerHTML = document.getElementById("inputP4Name").value;
+    document.getElementById("1Score").innerHTML = document.getElementById("inputP1Score").value;
+    document.getElementById("2Score").innerHTML = document.getElementById("inputP2Score").value;
+    document.getElementById("3Score").innerHTML = document.getElementById("inputP3Score").value;
+    document.getElementById("4Score").innerHTML = document.getElementById("inputP4Score").value;
 }
 
 function greenScreen() {
